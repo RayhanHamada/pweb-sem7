b546ffe67c5a021f21a667ffdaf2d2ddda42edc6
RayhanHamada <rayhanhamada1999@gmail.com>
2021-11-02 14:40:17 +0700

    feat: add m3

---
+++ m3/application/views/home.php
@@ -0,0 +1,90 @@
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal">Praktikum PWEB</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="#">Tambah Data</a>
+  </nav>
+  <a class="btn btn-outline-primary" href="<?= base_url('pweb/logout') ?>">Sign Out</a>
+</div>
+
+<div class="pricing-header px-3 py-1 pt-md-3 pb-md-4 mx-auto text-center">
+  <h1 class="display-4 pt-0">Tabel Nilai Kelas 4IAXX</h1>
+</div>
+
+<div class="container">
+  <table class="table table-hover mb-5">
+    <thead class="thead-light align-middle">
+      <tr>
+        <th rowspan="2" class="align-middle text-center">No.</th>
+        <th rowspan="2" class="align-middle text-center">Nama</th>
+        <th colspan="5" class="text-center">Nilai</th>
+        <th rowspan="2" class="align-middle text-center">Keterangan</th>
+        <th rowspan="2" class="align-middle text-center">Aksi</th>
+      </tr>
+      <tr>
+        <th class="text-center">Pert1</th>
+        <th class="text-center">Pert2</th>
+        <th class="text-center">Pert3</th>
+        <th class="text-center">Pert4</th>
+        <th class="text-center">Rata - rata</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th class="align-middle text-center">1</th>
+        <td class="align-middle">Mahasiswa 1</td>
+        <td class="align-middle text-center">80</td>
+        <td class="align-middle text-center">90</td>
+        <td class="align-middle text-center">70</td>
+        <td class="align-middle text-center">80</td>
+        <td class="align-middle text-center">80</td>
+        <td class="align-middle text-center">Lulus</td>
+        <td class="align-middle text-center">
+          <a href="" class="align-middle btn btn-success">Edit</a>
+          <a href="" class="align-middle btn btn-danger">Hapus</a>
+        </td>
+      </tr>
+      <tr class="align-middle bg-warning">
+        <th class="align-middle text-center">2</th>
+        <td class="align-middle">Mahasiswa 2</td>
+        <td class="align-middle text-center">0</td>
+        <td class="align-middle text-center">90</td>
+        <td class="align-middle text-center">60</td>
+        <td class="align-middle text-center">70</td>
+        <td class="align-middle text-center">55</td>
+        <td class="align-middle text-center">Tidak Lulus</td>
+        <td class="align-middle text-center">
+          <a href="" class="align-middle btn btn-success">Edit</a>
+          <a href="" class="align-middle btn btn-danger">Hapus</a>
+        </td>
+      </tr>
+      <tr>
+        <th class="align-middle text-center">3</th>
+        <td class="align-middle">Mahasiswa 3</td>
+        <td class="align-middle text-center">80</td>
+        <td class="align-middle text-center">90</td>
+        <td class="align-middle text-center">70</td>
+        <td class="align-middle text-center">80</td>
+        <td class="align-middle text-center">80</td>
+        <td class="align-middle text-center">Lulus</td>
+        <td class="align-middle text-center">
+          <a href="" class="align-middle btn btn-success">Edit</a>
+          <a href="" class="align-middle btn btn-danger">Hapus</a>
+        </td>
+      </tr>
+      <tr>
+        <th class="align-middle text-center">4</th>
+        <td class="align-middle">Mahasiswa 4</td>
+        <td class="align-middle text-center">80</td>
+        <td class="align-middle text-center">90</td>
+        <td class="align-middle text-center">70</td>
+        <td class="align-middle text-center">80</td>
+        <td class="align-middle text-center">80</td>
+        <td class="align-middle text-center">Lulus</td>
+        <td class="align-middle text-center">
+          <a href="" class="align-middle btn btn-success">Edit</a>
+          <a href="" class="align-middle btn btn-danger">Hapus</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
